2007b43e018b47ac41af7a185d04c5495046c92f
venkataprabhanjankumar <venkataprabhanjankumar@gmail.com>
2022-08-19 19:35:42 +0530

    updated tests

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -1,6 +1,8 @@
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 from tests.testcases import AuthenticateTestCases, TestCase
+from users import views
+from users.models import Post
 
 
 class TestLoginRequired(TestCase):
@@ -21,3 +23,49 @@ class TestLoginRequired(TestCase):
                 url = reverse(url_name)
                 response = self.client.get(url)
                 self.assertRedirectLoginRequired(response=response, url=url)
+
+
+class TestPostCreation(AuthenticateTestCases):
+    def setUp(self):
+        super().setUp()
+        url = reverse('create_book')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_url_resolves_correct_view(self):
+        view = resolve(reverse('create_post'))
+        self.assertEqual(view.func.view_class, views.CreatePost)
+
+    def test_html_content(self):
+        contents = (
+            ('<input type="hidden"', 1),  # for csrf
+            ('<input type="text"', 2),  # for name,description
+            ('<input type="number"', 1),  # for price
+        )
+        for content in contents:
+            with self.subTest(content=content[0]):
+                self.assertContains(self.response, content[0], content[1])
+
+
+class TestPostCreationSuccess(AuthenticateTestCases):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse('create_post')
+        self.data = {
+            'post_name': 'post1',
+            'post_description': 'Post Description 1',
+            'created_by': self.user.username
+        }
+        self.response = self.client.post(self.url, self.data)
+
+    def test_redirection(self):
+        self.assertEqual(self.response.url, reverse('list_posts'))
+        self.assertRedirects(self.response, reverse('list_posts'))
+
+    def test_post(self):
+        self.assertEqual(Post.objects.get(post_name='post1').post_name, 'post1')
